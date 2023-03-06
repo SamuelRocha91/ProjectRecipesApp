6118f95b9b259613ed8c296ea4a70bec1fef9684
Guilherme Valgas <guilhermehvalgas@gmail.com>
2023-03-06 15:37:58 -0300

    Implementação inicial do componente Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import mealsImg from '../images/mealIcon.svg';
+import drinksImg from '../images/drinkIcon.svg';
 
 function Footer() {
   return (
-    <div>
-      <span>Footer</span>
-    </div>
+    <footer
+      data-testid="footer"
+    >
+      <Link to="/meals">
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealsImg }
+          alt="meals-icon"
+        />
+      </Link>
+      <Link to="/drinks">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinksImg }
+          alt="drinks-icon"
+        />
+      </Link>
+    </footer>
   );
 }
 
