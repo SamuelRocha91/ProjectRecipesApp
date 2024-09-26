1934eadf2f040b0b1e42359c753ed0a8b89232a4
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-09-25 23:05:41 -0300

    feat: estiliza lista de ingredientes

--- src/components/ListIngredients.jsx
+++ src/components/ListIngredients.jsx
@@ -1,20 +1,23 @@
 import { useContext } from 'react';
 import RecipesContext from '../context/RecipesContext';
+import './ListIngredientes.css';
 
 function ListIngredients() {
   const { foodDetails } = useContext(RecipesContext);
 
   return (
-    <ul>
+    <ul className="u-list">
       {foodDetails !== null && foodDetails.ingredients.map((ingredient, index) => (
         <label
+          className="label-ingredients"
           key={ `${foodDetails.instructions[index][1]}${index}` }
         >
           <li
+            className="list-it"
             type="checkbox"
           >
             { ingredient[1]}
-            <span>
+            <span className="span-ingredients">
               {' '}
               {foodDetails.instructions[index][1] !== null
             && `${foodDetails.instructions[index][1]}` }
